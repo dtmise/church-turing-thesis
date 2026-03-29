9fcf839db0f91409d65c5d6e7f1ca85e344939da
Dtmise <dtmise@atomicmail.io>
2026-03-29 15:43:06 +0700

    Added the use of HTTPS in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 1904;
+let server;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+if (process.env.NODE_ENV == 'production') {
+    try {
+        const https   = await import('https');
+        const fs      = await import('fs');
+        const options = {
+            key: fs.readFileSync('./sns/server.key'),
+            cert: fs.readFileSync('./sns/server.cert')
+        };
+        server = https.createServer(options, app);
+    } catch (err) {
+        throw new Error('Https is not allowed. ' + err.message);
+    }
+} else {
+    const http = await import('http');
+    server = http.createServer(app);
+}
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+    console.log(`Server running on localhost:${PORT}`);
 });
